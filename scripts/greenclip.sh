10f5095c9bb305acee36b4f7c6543d9df7c0a14c
Carlos Rosquillas <crosquillas@gmail.com>
2022-02-16 16:48:54 -0800

    Make greenclip executable

--- scripts/greenclip.sh
+++ scripts/greenclip.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 VERSION="v4.2"
-GREENCLIP_BIN="~/bin/greenclip"
+BIN_PATH="~/.dotfiles/bins/bin/"
+GREENCLIP_BIN="${BIN_PATH}/greenclip"
 
 # If greenclip file doesn't exist, download it
 if [ ! -f $GREENCLIP_BIN ]; then
   echo "Installing Greenclip for Rofi"
-  wget https://github.com/erebe/greenclip/releases/download/${VERSION}/greenclip -P ~/.dotfiles/bins/bin/
+  wget https://github.com/erebe/greenclip/releases/download/${VERSION}/greenclip -P $BIN_PATH
+  chmod +x $GREENCLIP_BIN
 fi
