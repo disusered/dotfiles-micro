cc5b166d1aaf83b8f645035bf91e146ad29ddabf
Carlos Rosquillas <crosquillas@gmail.com>
2022-02-16 16:29:01 -0800

    Use user bin path included with Linux

--- scripts/greenclip.sh
+++ scripts/greenclip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="v4.2"
-GREENCLIP_BIN="~/.dotfiles/.local/bin/greenclip"
+GREENCLIP_BIN="~/.dotfiles/bin/greenclip"
 
 # If greenclip file doesn't exist, download it
 if [ ! -f $GREENCLIP_SYMLINK ]; then
