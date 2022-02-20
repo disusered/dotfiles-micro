a259df968b2a2777b0affbbf45a45e3c1ed19a85
Carlos Rosquillas <crosquillas@gmail.com>
2022-02-20 14:37:10 -0800

    Try to fix greenclip detection

--- scripts/greenclip.sh
+++ scripts/greenclip.sh
@@ -5,7 +5,7 @@ BIN_PATH="~/.dotfiles/bins/bin/"
 GREENCLIP_BIN="${BIN_PATH}/greenclip"
 
 # If greenclip file doesn't exist, download it
-if [ ! -f $GREENCLIP_BIN ]; then
+if [ ! -f "${GREENCLIP_BIN}" ]; then
   echo "Installing Greenclip for Rofi"
   wget https://github.com/erebe/greenclip/releases/download/${VERSION}/greenclip -P $BIN_PATH
   chmod +x $GREENCLIP_BIN
