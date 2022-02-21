d5d99fefdac4cfd474eb7c2210d65be5a63de6ab
Carlos Rosquillas <crosquillas@gmail.com>
2022-02-20 16:07:46 -0800

    Add lazygit to dotfiles

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,6 +3,7 @@
 # If redhat/fedora, else if ubuntu
 if [ -f /etc/redhat-release ]; then
   log "Installing dependencies for RedHat systems"
+  sudo dnf copr enable atim/lazygit -y
   sudo dnf install -y $(cat $HOME/.dotfiles/packages/dnf)
 elif [ -f /etc/lsb-release ]; then
   log "Installing dependencies for Ubuntu systems"
