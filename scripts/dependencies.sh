426718bfe7e145be5ddeeddfc2a5588218fb930a
Carlos Rosquillas <crosquillas@gmail.com>
2022-04-19 19:42:01 -0700

    Add cloud dependencies

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -16,6 +16,22 @@ if [ -f /etc/redhat-release ]; then
   sudo dnf config-manager --add-repo \
     https://cli.github.com/packages/rpm/gh-cli.repo
 
+  sudo dnf config-manager --add-repo \
+    https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+
+  # gcloud
+  # https://cloud.google.com/sdk/docs/install
+  sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+
   # additional sources
   sudo dnf copr enable atim/lazygit -y
 
