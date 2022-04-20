0ddc2d45f12383f064547d1c805a480cc45ecd44
Carlos Rosquillas <crosquillas@gmail.com>
2022-04-19 22:08:51 -0700

    Autoformat plugins lua

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,5 +1,5 @@
-local packer = require'packer'
-local util = require'packer.util'
+local packer = require 'packer'
+local util = require 'packer.util'
 
 packer.init({
   package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack'),
@@ -9,8 +9,8 @@ packer.init({
 })
 
 -- Install LuaRocks integration
-require'packer.luarocks'.setup_paths(util.join_paths(vim.fn.stdpath('cache'), 'packer_hererocks', '2.1.0-beta3'))
-require'packer.luarocks'.install_commands()
+require 'packer.luarocks'.setup_paths(util.join_paths(vim.fn.stdpath('cache'), 'packer_hererocks', '2.1.0-beta3'))
+require 'packer.luarocks'.install_commands()
 
 return packer.startup(function(use)
 
@@ -50,10 +50,10 @@ return packer.startup(function(use)
   -----------------------------------------------------------------------------
 
   -- Easily manipulate parentheses, brackets, quotes, XML tags, and more
-  use {'tpope/vim-surround', requires = {'tpope/vim-repeat'}}
+  use { 'tpope/vim-surround', requires = { 'tpope/vim-repeat' } }
 
   -- Handy bracket mappings
-  use {'tpope/vim-unimpaired', requires = {'tpope/vim-repeat'}}
+  use { 'tpope/vim-unimpaired', requires = { 'tpope/vim-repeat' } }
 
   -- Colorize hex/rgba/hsla codes
   use 'norcalli/nvim-colorizer.lua'
@@ -62,14 +62,14 @@ return packer.startup(function(use)
   use { 'nvim-treesitter/nvim-treesitter' }
 
   -- Allow commenting current line, visual selection or motion
-  use {'JoosepAlviste/nvim-ts-context-commentstring', requires = {'b3nj5m1n/kommentary'}}
+  use { 'JoosepAlviste/nvim-ts-context-commentstring', requires = { 'b3nj5m1n/kommentary' } }
 
   -- EasyMotion-like movement
   -- use 'justinmk/vim-sneak'
-  use {'phaazon/hop.nvim', as = 'hop'}
+  use { 'phaazon/hop.nvim', as = 'hop' }
 
   -- EnMasse
-  use {'Wolfy87/vim-enmasse'}
+  use { 'Wolfy87/vim-enmasse' }
 
   -----------------------------------------------------------------------------
   -- Language & Syntax
@@ -79,13 +79,13 @@ return packer.startup(function(use)
   use 'elixir-editors/vim-elixir'
 
   -- Markdown
-  use {'iamcco/markdown-preview.nvim', run = function() vim.fn['mkdp#util#install'](0) end}
+  use { 'iamcco/markdown-preview.nvim', run = function() vim.fn['mkdp#util#install'](0) end }
 
   -- LaTex
-  use {'lervag/vimtex'}
+  use { 'lervag/vimtex' }
 
   -- CoPilot
-  use {'github/copilot.vim'}
+  use { 'github/copilot.vim' }
 
   -----------------------------------------------------------------------------
   -- IDE plugins
@@ -104,7 +104,7 @@ return packer.startup(function(use)
   use 'tpope/vim-fugitive'
 
   -- Improved match with %
-  use {'andymass/vim-matchup', event = 'VimEnter'}
+  use { 'andymass/vim-matchup', event = 'VimEnter' }
 
   -- Automatically close tags using TreeSitter
   use { 'windwp/nvim-ts-autotag' }
@@ -113,17 +113,17 @@ return packer.startup(function(use)
   use 'jeetsukumaran/vim-filebeagle'
 
   -- File picker
-  use { 'camspiers/snap', rocks = {'fzy'}}
+  use { 'camspiers/snap', rocks = { 'fzy' } }
 
   -- Completion
   use {
     'hrsh7th/nvim-compe',
     requires = {
-      {'hrsh7th/vim-vsnip'},
-      {'hrsh7th/vim-vsnip-integ'},
-      {'rafamadriz/friendly-snippets'},
-      {'ray-x/lsp_signature.nvim'},
-      {'onsails/lspkind-nvim'}
+      { 'hrsh7th/vim-vsnip' },
+      { 'hrsh7th/vim-vsnip-integ' },
+      { 'rafamadriz/friendly-snippets' },
+      { 'ray-x/lsp_signature.nvim' },
+      { 'onsails/lspkind-nvim' }
     }
   }
 
@@ -133,13 +133,13 @@ return packer.startup(function(use)
   -- Enable GitGutter like signs
   use {
     'lewis6991/gitsigns.nvim',
-    requires = {'nvim-lua/plenary.nvim'},
+    requires = { 'nvim-lua/plenary.nvim' },
     tag = 'release'
   }
 
   -- DBA UI
   use {
     'tpope/vim-dadbod',
-    requires = {'kristijanhusak/vim-dadbod-ui'}
+    requires = { 'kristijanhusak/vim-dadbod-ui' }
   }
 end)
