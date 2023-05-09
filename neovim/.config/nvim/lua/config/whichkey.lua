898bbb9a110dcdd54759adb046ac891129b0f514
Carlos Rosquillas <crosquillas@gmail.com>
2023-05-09 12:46:03 -0700

    Update WhichKey config

--- neovim/.config/nvim/lua/config/whichkey.lua
+++ neovim/.config/nvim/lua/config/whichkey.lua
@@ -1,3 +1,9 @@
+-- Enable delay before which-key appears
+vim.o.timeout = true
+
+-- Delay before which-key appears after pressing a key
+vim.o.timeoutlen = 300
+
 require("which-key").setup {
   -- Shows WhichKey when pressing z= to select spelling suggestions
   spelling = {
